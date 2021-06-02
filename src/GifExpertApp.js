35c6d6b07096469b41125865cf1a4d783d8a19cd
Santiago <tiagosuarez22@gmail.com>
2021-06-02 14:59:14 -0500

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import {AddCategory} from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp=()=>{
+
+    const [categories, setCategories] = useState(['space'])
+
+    // const handleAdd=()=>{
+    //     /**
+    //      * 1 - Obtenemos las categorias del primer estado con ...categories
+    //      * y agregamos la nueva categoria (Space)
+    //      */
+
+    //     // const newCategories=[...categories, 'Space'];
+    //     // setCategories(newCategories);
+
+    //     /**
+    //      * 2 - Obtenemos el estado anterior por medio de un callback
+    //      * y agregamos la nueva categoria (Space)
+    //      */
+    //     setCategories((catgs)=>{
+    //         return [...catgs, 'space'];
+    //     });
+    // }
+
+    return(
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
+            <hr/>
+                {
+                    categories.map((category)=>{
+                        return(
+                            <GifGrid 
+                                key={category} 
+                                category={category} 
+                            />
+                        )
+                    })
+                }
+        </>
+    );
+}
+
+export default GifExpertApp;
